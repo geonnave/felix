c4a61c81a25fdec15017ab886415d4955dce4794
Geovane Fedrecheski <geonnave@gmail.com>
2018-03-21 13:07:31 -0300

    Add tcp server
    Topics covered:
    - module attributes
    - require
    - default arguments
    - interoperability with erlang functions
    - :gen_tcp
    - tuples
    - recursion
    
    Other topics:
    - brief on OSI layers
    
    Topics reinforced
    - pattern matching (with tuples)
    - string interpolation
    - pipe operator
    
    Extras:
    - pure vs non-pure functions (compare Handler and ElixServer)

--- lib/elix_server.ex
+++ lib/elix_server.ex
@@ -1,18 +1,36 @@
 defmodule ElixServer do
-  @moduledoc """
-  Documentation for ElixServer.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  @tcp_options [:binary, active: false, reuseaddr: true]
 
-  ## Examples
+  def start(port \\ 2222) do
+    {:ok, socket} = :gen_tcp.listen(port, @tcp_options)
 
-      iex> ElixServer.hello
-      :world
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
 
-  """
-  def hello do
-    :world
+  def loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    client
+    |> receive_request()
+    |> Handler.handle()
+    |> send_response(client)
+
+    loop_acceptor(socket)
+  end
+
+  def receive_request(client) do
+    {:ok, request} = :gen_tcp.recv(client, 0)
+    Logger.info("Received #{inspect(request)}")
+    request
+  end
+
+  def send_response(response, client) do
+    Logger.info("Sending back #{inspect(response)}")
+    :gen_tcp.send(client, response)
+    :gen_tcp.close(client)
   end
 end
+
