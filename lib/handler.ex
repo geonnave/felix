4b3e33bb5c08044ef3ddf5eae4e65d2ebc8dd425
Geovane Fedrecheski <geonnave@gmail.com>
2018-03-21 12:52:48 -0300

    Use context struct to make status configurable
    Topics covered:
    - variable binding when pattern matching in functions
    - immutability
    - maps
      - put_in
    
    Topics reinforced:
    - maps
    - pipe operator

--- lib/handler.ex
+++ lib/handler.ex
@@ -7,20 +7,32 @@ defmodule Handler do
     |> build_resp()
   end
 
-  def route(%{method: method, path: path}) do
-    match(method, path)
+  def route(context = %{method: method, path: path}) do
+    match(method, path, context)
   end
 
-  def match("GET", "/value"), do: 42
-  def match("GET", "/hello"), do: "world"
-  def match(method, path), do: "#{method} #{path} not found"
+  def match("GET", "/value", context) do
+    context
+    |> put_in([:resp_body], 42)
+    |> put_in([:status], "200 Ok")
+  end
+  def match("GET", "/hello", context) do
+    context
+    |> put_in([:resp_body], "world")
+    |> put_in([:status], "200 Ok")
+  end
+  def match(method, path, context) do
+    context
+    |> put_in([:resp_body], "#{method} #{path} not found")
+    |> put_in([:status], "404 Not Found")
+  end
 
-  def build_resp(resp_body) do
+  def build_resp(context) do
     """
-    HTTP/1.1 200 Ok\r
+    HTTP/1.1 #{context[:status]}\r
     Content-type: text/plain\r
 \r
-    #{resp_body}
+    #{context[:resp_body]}
     """
   end
 
