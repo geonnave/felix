6906111a9923a9f98fc9286ccb836f123ccfac50
Geovane Fedrecheski <geonnave@gmail.com>
2018-04-03 00:22:01 -0300

    Change BrandsModel to become more interesting
    Topics reinforced:
    - maps
    - genserver

--- lib/sample_app/models/brand_model.ex
+++ lib/sample_app/models/brand_model.ex
@@ -1,19 +1,18 @@
 defmodule SampleApp.BrandModel do
   def start do
-    brands = ["Tesla", "Ford"]
-    TOP.GenServer.start(__MODULE__, &handle_call/2, &handle_cast/2, brands)
+    brands = %{
+      "Tesla" => ["Model S", "Model X", "Model 3"],
+      "Ford" => ["Focus", "Fiesta", "Fusion"]
+    }
+    TOP.GenServer.start(__MODULE__, &handle_call/2, nil, brands)
   end
 
   def get_brands do
     TOP.GenServer.call __MODULE__, :get_brands
   end
 
-  def handle_call(:get_brands, brands) do
-    {brands, brands}
-  end
-
-  def handle_cast(_, state) do
-    state
+  def handle_call(:get_brands, state) do
+    {Map.keys(state), state}
   end
 end
 
